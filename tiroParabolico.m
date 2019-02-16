31182423ba1a172a5967a13da9dfd4d4ad2f1450
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2019-02-16 17:15:52 -0600

    bugs

--- tiroParabolico.m
+++ tiroParabolico.m
@@ -1,10 +1,9 @@
-% Los comentarios serán el algoritmo
+%El dato de gravedad es una constante g = 9.81m/s^2
+g = 9.81;
 disp('Programa para calcular la altura máxima y el alcance de un objeto en el tiro parabálico.'); 
 %1. Pedir los datos de velocidad inicial (en metros) y el ángulo.
 vi = input('Velocidad incial (m) : ');
 a = input('Angulo del tiro (grados): ');
-%El dato de gravedad es una constante g = 9.81m/s^2
-g = 9.81;
 %2. Ingresarlos en las formulas correspondientes
 ymax = (vi^2)*(sin(a*pi/180)^2)/(2*g); 
 xmax = ((vi^2)*(sin(a*pi/180)*2))/g;
