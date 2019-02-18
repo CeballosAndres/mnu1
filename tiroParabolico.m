221a8d99674f8ccc1e56dc743c51e7d3585ea0b5
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2019-02-18 16:23:26 -0600

    cambios beverly

--- tiroParabolico.m
+++ tiroParabolico.m
@@ -2,16 +2,34 @@
 g = 9.81;
 disp('Programa para calcular la altura máxima y el alcance de un objeto en el tiro parabálico.'); 
 %1. Pedir los datos de velocidad inicial (en metros) y el ángulo.
-vi = input('Velocidad incial en metros (m/s) : ');
+vi = input('Velocidad incial (m) : ');
 a = input('Angulo del tiro (grados): ');
 %2. Ingresarlos en las formulas correspondientes
-ymax = (vi * sin(a*pi/180))^2/(2*g); 
-xmax = (vi^2)*(sin(2*a*pi/180))/g;
+angulo=a*pi/180;
+ymax = (vi^2)*(sin(angulo)^2)/(2*g); 
+xmax = (vi^2)*(sin((angulo)*2))/g;
+
+tiempo=(2*vi*sin(angulo))/g;
+t=[0:tiempo/100:tiempo];
+
+x=vi*cos(angulo)*t;
+y=vi*t*sin(angulo)-(g*t.**2)/2;
+%grafica distancia (x)/altura
+plot(x,y);
+
+%grafica tiempo/distancia
+%grafica tiempo/altura
+%plot(t,x);
+%hold on
+%plot(t,y);
+
 
 %3. Mostrar los valores
-printf('\n *Alcance del objeto: %f m\n *Altura maxima: %f m\n', xmax, ymax); 
+printf('\n *Alcance del objeto: %f m\n *Altura maxima: %f m\n*tiempo  en el aire: %f s', xmax, ymax,tiempo); 
+
+
 
 %% DATOS A INGRESAR: 
 % V = 8.3
 % A = 28
-%SALIDAS *Alcance del objeto: 5.821855 m *Altura maxima: 0.773884 m
+%SALIDAS : altura: 0.773   alcance: 3.804832
